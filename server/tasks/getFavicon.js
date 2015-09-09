41bb448b6e7184434a31e9770b26e6cd2b614496
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-08 19:14:43 -0700

    Check two different link rel types for favicons now

--- server/tasks/getFavicon.js
+++ server/tasks/getFavicon.js
@@ -11,8 +11,15 @@ module.exports = function(products) {
 var getFavicon = function(productModel) {
   return request.getAsync(productModel[0].product_url)
     .spread(function(response, body) {
+      var favicon = null;
+
       var $ = cheerio.load(body);
-      var favicon = $('link[rel="shortcut icon"]').attr('href');
+      var faviconStrings = ['link[rel="shortcut icon"]', 
+                            'link[rel="icon"]'];
+      for(var i = 0; i < faviconStrings.length; i++) {
+        var guess = $(faviconStrings[i]).attr('href');
+        favicon = (guess) ? guess: null;
+      }  
 
       if(favicon) {
         productModel[0].favicon_url = favicon;
