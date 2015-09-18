5e43d6df02b3b3e755f52df023f27417d041a696
Erik Barzdukas <erik.bardzdukas@gmail.com>
2015-09-18 11:11:28 -0700

    Troubleshoot logging

--- server/products/productController.js
+++ server/products/productController.js
@@ -61,6 +61,7 @@ module.exports = {
         return result.slice(offset, offset + limit);
       })
       .then(function(result) {
+        console.log(result);
         res.set({'Content-Type': 'application/json'});
         res.status(200).send(JSON.stringify(result));
       })
